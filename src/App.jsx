e25d02fba9409de60e5d5ab944952dfd89340dbf
Doddy MATABARO <matabarododdy@gmail.com>
2022-08-29 08:28:59 +0200

    hash spotify  token

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,40 @@ import Profile from './component/Profile';
 import SingIn from './component/SignIn';
 
 function App() {
+  const CLIENT_ID = "9b707ef30ad74876b0ebded1d118b9ce"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
   const [logIn, setLogIn] = useState(false)
 
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+        setToken(token)
+    }, [])
+
+      const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+     }
   return (
     <div className="App">
         <Header/>
-             { logIn ? 
+             {/* { logIn ? 
                   <SingIn  clientGoogleId={clientGoogleId} /> 
                 :
                   <Profile />
-             }
+             } */}
  
         <Footer />
     </div>
