e479e3d22fe680bc2e4d6e6cb671a7a2e9d858b5
Doddy MATABARO <77513342+DoddyMatabaro@users.noreply.github.com>
2022-11-23 14:41:23 +0200

    Update index.jsx

--- src/component/SignIn/index.jsx
+++ src/component/SignIn/index.jsx
@@ -3,7 +3,7 @@ import Header from '../Header'
 import './signIn.css'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
-// import { GoogleLogin } from 'react-google-login';
+
 import { gapi } from 'gapi-script'; 
 
 const SingIn = () => {
@@ -13,43 +13,11 @@ const SingIn = () => {
   const scope = process.env.scope;
   
   const handleClick = async () => {
-    // const CLIENT_ID = "9b707ef30ad74876b0ebded1d118b9ce"
-    // const REDIRECT_URI = "http://localhost:5174/callback/"
-    // const api_uri = "https://accounts.spotify.com/authorize";
-    // const scope = [
-    //   "user-read-private",
-    //   "user-read-email",
-    //   "user-modify-playback-state",
-    //   "user-read-playback-state",
-    //   "user-read-currently-playing",
-    //   "user-read-recently-played",
-    //   "user-top-read",
-    // ];
     window.location.href = `${api_uri}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scope.join(
       " "
     )}&response_type=token&show_dialog=true`;
   };
-      // useEffect(() => { //initialize gapi and client ID
-      // const initClient = () => {
-      //       gapi.client.init({
-      //       clientId: clientGoogleId,
-      //       scope: ''
-      //     });
-      //  };
-      //  gapi.load('client:auth2', initClient);
-      // });
-  
-      // const onSuccess=()=>{
-      //   console.log('====================================');
-      //   console.log('success:', res);
-      //   console.log('====================================');
-      // }
-  
-      // const onFailure=()=>{
-      //   console.log('====================================');
-      //   console.log('Failed:', err);
-      //   console.log('====================================');
-      // }
+      
   return (
     <div className=''>
         <div className='sign-in-content'>
@@ -62,21 +30,7 @@ const SingIn = () => {
                   <button  className="btn btn-login" onClick={handleClick}>Connect Spotify GDA</button>
                     {/* <a className='btn btn-primary spotify-btn' href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>LOGIN TO SPOTIFY GDA</a>            */}
               </div>
-              {/* <article> */}
-                    {/* <GoogleLogin 
-                      clientId={clientGoogleId}
-                      buttonText="Sign in with Google"
-                      onSuccess={onSuccess}
-                      onFailure={onFailure}
-                      cookiePolicy={'single_host_origin'}
-                      isSignedIn={true}
-                      className="google-login"
-                    /> */}
-                    {/* <p>By signing up in our spotify GDA app , 
-                          you agree with
-                        our <a href='#'>Terms & Conditions</a> and <a href='#'>Privacy Policy</a>. 
-                    </p> */}
-              {/* </article> */}
+             
         </div>
     </div>
   )
